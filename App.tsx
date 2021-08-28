aff10640aa2abd311c73f1893e7f32bc37999551
Dan Shrout <danshrout1@gmail.com>
2021-08-28 14:53:55 -0700

    Installed react-native-safe-area-context library and built SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function App() {
   return (
+    <SafeAreaView>
     <View style={styles.container}>
       <Text>Just getting started :)</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
